7a18c1e843b68674dcb5d81fe559f0ed63d13e5f
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-30 19:28:49 -0300

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CompWinPredictor
+
+## Tarefas Concluídas
+
+- [x] Implementar funções para configurar e gerenciar o navegador Selenium.
+- [x] Melhorar a estrutura do scraper.
+- [x] Identificar e lidar com partidas que são remakes.
+- [x] Utilizar informações constantes e de configurações dinamicamente dos arquivos `settings.py` e `constants.py`
+
+## Tarefas Pendentes
+
+- [ ] Criar uma base de dados SQLite para armazenar informações das partidas.
+- [ ] Implementar a função para inserir informações do histórico de partidas no banco de dados.
+- [ ] Lidar com exceções e erros de forma mais abrangente.
+- [ ] Refatorar o código para melhor legibilidade e manutenibilidade.
+- [ ] Considerar o uso de arquivos de configuração ou variáveis de ambiente para configurar informações como o caminho do driver do Chrome.
+- [ ] Pensar em como armazenar partidas únicas, ou como criar um identificador único para cada partida, etc...
---
+++ database/database.py
@@ -0,0 +1,34 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """Create a database connection to the SQLite database specified by db_file."""
+    connection = None
+    
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+        return connection
+
+def create_table(connection, query):
+    """Create a table from the create_table_sql statement."""
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return "Table created"
+    except Error as e:
+        print(e)
+        return
+    
+def insert_data(connection, query, data):
+    """Insert data into the table using the insert_sql statement."""
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query, data)
+        connection.commit()
+        return "Data inserted"
+    except Error as e:
+        print(e)
+        return
